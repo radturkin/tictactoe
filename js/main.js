2c1c15e1e47614f036b7ee44d202cce3eb260cf5
Rad <bturkin@gmail.com>
2022-08-05 14:15:23 -0500

    reads values

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@
 let countClick=0
 localStorage.setItem("counter", countClick)
 let squareArray=["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
-
+let a1 = document.getElementById("a1")
+let a2 = document.getElementById("a2")
+let a3 = document.getElementById("a3")
+let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
+let b3 = document.getElementById("b3")
+let c1 = document.getElementById("c1")
+let c2 = document.getElementById("c2")
+let c3 = document.getElementById("c3")
 //the function that places the xs and os
 const onClick = function(countClick) {
-    console.log(this.id, this.innerHTML);
     let place = this.id
 
     countClick = Number(localStorage.getItem("counter"))
@@ -19,9 +26,12 @@ const onClick = function(countClick) {
         playerValue="O"
     }
     
-    document.getElementById(place).innerText = playerValue
+    document.getElementById(place).innerHTML = playerValue
+    console.log(this.id, this.innerHTML);
     countClick+=1
     localStorage.setItem("counter", countClick)
+
+    winner()
 
     return countClick
   }
@@ -39,20 +49,14 @@ document.getElementById("c3").onclick = onClick
 
 
 //check for winner
-let a1 = document.getElementById("a1")
-let a2 = document.getElementById("a2")
-let a3 = document.getElementById("a3")
-let b1 = document.getElementById("b1")
-let b2 = document.getElementById("b2")
-let b3 = document.getElementById("b3")
-let c1 = document.getElementById("c1")
-let c2 = document.getElementById("c2")
-let c3 = document.getElementById("c3")
 
-if (a1.value == a2.value){
-    console.log("winner")
-    //document.getElementById("win").innerText = "winner"
+function winner(){
+    console.log(a1.innerHTML, a2.innerHTML, a3.innerHTML)
 
+    if ((a1.innerHTML == a2.innerHTML == a3.innerHTML)&&(a1.innerHTML!="")){
+        console.log("winner")
+        document.getElementById("win").innerHTML = "winner"
+        }
 }
 //reset button resets game
 function resetGame(){
