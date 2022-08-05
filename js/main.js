099cfee84362b65d2f4ca9e70a30e5e351124938
Rad <bturkin@gmail.com>
2022-08-05 13:54:22 -0500

    reest

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
-//HI I am working on the tic tac toe projcet from class 30 
-//please join me
 //countClick for alternating xs and os
 let countClick=0
 localStorage.setItem("counter", countClick)
+let squareArray=["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
 
 //the function that places the xs and os
 const onClick = function(countClick) {
@@ -38,7 +37,28 @@ document.getElementById("c1").onclick = onClick
 document.getElementById("c2").onclick = onClick
 document.getElementById("c3").onclick = onClick
 
+
 //check for winner
+let a1 = document.getElementById("a1")
+let a2 = document.getElementById("a2")
+let a3 = document.getElementById("a3")
+let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
+let b3 = document.getElementById("b3")
+let c1 = document.getElementById("c1")
+let c2 = document.getElementById("c2")
+let c3 = document.getElementById("c3")
 
-//reset button
+if (a1.value == a2.value){
+    console.log("winner")
+    //document.getElementById("win").innerText = "winner"
 
+}
+//reset button resets game
+function resetGame(){
+    localStorage.clear()
+    for(let i=0;i<9;i++){
+        document.getElementById(squareArray[i]).innerText = ""
+    }return countClick
+}
+document.querySelector("button").onclick = resetGame
